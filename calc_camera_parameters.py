12771064df51647d007da92621f848b4cfeffa78
Valentin Bruder <vbruder@gmail.com>
2020-02-26 15:06:38 +0100

    added projection plane

--- calc_camera_parameters.py
+++ calc_camera_parameters.py
@@ -71,7 +71,7 @@ def calc_pos_diagonal(fovy, bbox_min, bbox_max, num_iterations, step, name):
     # set_look_to(bbox_max)
     diagonal_length = np.linalg.norm(bbox_max - bbox_min)
     camera_dist = (diagonal_length * 0.5) / math.tan(fovy * 0.5 * math.pi / 180.)
-    camera_dist *= (1. - step / num_iterations)
+    camera_dist *= (1. - step / float(num_iterations))
     cam_pos = bbox_min + (bbox_max - bbox_min)*0.5 + (begin - end)/diagonal_length * camera_dist
     ray = end
 
@@ -138,18 +138,90 @@ def calc_pos(fovy, bbox_min, bbox_max, num_iterations, step, name):
         cam_pos, ray = calc_pos_path(fovy, bbox_min, bbox_max, num_iterations, step, name)
     return cam_pos, ray
 
+''' 
+Fit a plane to a set of points in 3D (minimize the distances).
+@see https://stackoverflow.com/questions/35070178/fit-plane-to-a-set-of-points-in-3d-scipy-optimize-minimize-vs-scipy-linalg-lsts 
+'''
+def get_plane(coords):
+    # barycenter of the points
+    # compute centered coordinates
+    G = coords.sum(axis=0) / coords.shape[0]
 
+    # run SVD
+    u, s, vh = np.linalg.svd(coords - G)
+
+    # unitary normal vector
+    u_norm = vh[2, :]
+    return u_norm
+
+'''
+plot the plane from 
+@see https://stackoverflow.com/questions/3461869/plot-a-plane-based-on-a-normal-vector-and-a-point-in-matlab-or-matplotlib
+'''
+from mpl_toolkits.mplot3d import axes3d
+from matplotlib.patches import Circle, PathPatch
+import matplotlib.pyplot as plt
+from matplotlib.transforms import Affine2D
+from mpl_toolkits.mplot3d import art3d
+
+def rotation_matrix(d):
+    sin_angle = np.linalg.norm(d)
+    if sin_angle == 0:return np.identity(3)
+    d /= sin_angle
+    eye = np.eye(3)
+    ddt = np.outer(d, d)
+    skew = np.array([[    0,  d[2],  -d[1]],
+                  [-d[2],     0,  d[0]],
+                  [d[1], -d[0],    0]], dtype=np.float64)
+
+    M = ddt + np.sqrt(1 - sin_angle**2) * (eye - ddt) + sin_angle * skew
+    return M
+
+def pathpatch_2d_to_3d(pathpatch, z, normal):
+    if type(normal) is str: #Translate strings to normal vectors
+        index = "xyz".index(normal)
+        normal = np.roll((1.0,0,0), index)
+
+    normal /= np.linalg.norm(normal) #Make sure the vector is normalised
+    path = pathpatch.get_path() #Get the path and the associated transform
+    trans = pathpatch.get_patch_transform()
+
+    path = trans.transform_path(path) #Apply the transform
+
+    pathpatch.__class__ = art3d.PathPatch3D #Change the class
+    pathpatch._code3d = path.codes #Copy the codes
+    pathpatch._facecolor3d = pathpatch.get_facecolor #Get the face color    
+
+    verts = path.vertices #Get the vertices in 2D
+
+    d = np.cross(normal, (0, 0, 1)) #Obtain the rotation vector    
+    M = rotation_matrix(d) #Get the rotation matrix
+
+    pathpatch._segment3d = np.array([np.dot(M, (x, y, 0)) + (0, 0, z) for x, y in verts])
+
+def pathpatch_translate(pathpatch, delta):
+    pathpatch._segment3d += delta
+
+def plot_plane(ax, point, normal, size=10, color='y'):    
+    p = Circle((0, 0), size, facecolor = color, alpha = .2)
+    ax.add_patch(p)
+    pathpatch_2d_to_3d(p, z=0, normal=normal)
+    pathpatch_translate(p, (point[0], point[1], point[2]))
+
+'''
+driver code
+'''
 def main():
     fovy = 60   # always 60 degrees for the point data benchmark
     bbox_min = np.array([-1, -1, -1])   # depends on the data set
     bbox_max = np.array([ 1,  1,  1])   # depends on the data set
-    num_iterations = 12
     camera_path_names = ["diagonal_x", "diagonal_y", "diagonal_z", 
                          "orbit_x", "orbit_y", 
                          "path_x", "path_y", "path_z", 
                          "path_sin_x", "path_sin_y", "path_sin_z"
                         ]
 
+    num_iterations = 12
     # read number of iterations from console
     if len(sys.argv) > 1:
         num_iterations = int(sys.argv[1])
@@ -161,15 +233,25 @@ def main():
         ray_x = []
         ray_y = []
         ray_z = []
+
+        coords = []
+        rays = []
+        
         print('~~~~~ ' + name)
         for i in range(num_iterations):
             pos, ray = calc_pos(fovy, bbox_min, bbox_max, num_iterations, i, name)
+            
             pos_x.append(pos[0])
             pos_y.append(pos[1])
             pos_z.append(pos[2])
+            coords.append(pos)
+            # np.append([coords], [pos], axis=0)
+
             ray_x.append(ray[0])
             ray_y.append(ray[1])
             ray_z.append(ray[2])
+            rays.append(ray)
+
             print('pos: ' + str(pos))
             print('dir: ' + str(ray))
 
@@ -189,6 +271,12 @@ def main():
         for s, e in combinations(np.array(list(product(r,r,r))), 2):
             if np.sum(np.abs(s-e)) == r[1]-r[0]:
                 ax.plot3D(*zip(s, e), color="red")
+
+        # calculate projection plane
+        normal = get_plane(np.array(coords))
+        print('normal: ' + str(normal))
+        point = coords[len(coords)/2]
+        plot_plane(ax, point, normal, size=4) 
 
         plt.title(name)
         plt.show()
